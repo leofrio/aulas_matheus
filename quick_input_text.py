8c067c68ce3801d13f327e10b468172eb46c087c
Leonardo Pontes Gaspar <leogaspar01@hotmail.com>
2025-08-05 19:29:12 -0300

    adiocionando mudancas

---
+++ quick_input_text.py
@@ -0,0 +1,3 @@
+numero_magico = float(input("me diga um numero:")  )
+print(f"esse é o numero que voce digitou: {numero_magico}") 
+print(type(numero_magico))
--- solucoes exercicios/17.py
+++ solucoes exercicios/17.py
@@ -1,22 +1,28 @@
 import math 
 
-square_meters_qtd = float(input("diga a quantidade de m2 a serem pintados:")) 
+metros_quadrados = float(input("diga a quantidade de m2 a serem pintados:")) 
 
 redimento_litro = 6 
- 
+  
+
+# 10/5 = 2.5
+# math.ceil(10/5) =  3
+# math.floor(10/5) =  2
+
+
 lata_litro = 18  
 galao_litro = 3.6  
 lata_preco = 80 
 galao_preco = 25
 lata_rendimento = lata_litro * redimento_litro 
 galao_rendimento = galao_litro * redimento_litro 
-apenas_lata = math.ceil(square_meters_qtd/lata_rendimento) 
+apenas_lata = math.ceil(metros_quadrados/lata_rendimento) 
 apenas_lata_preco = apenas_lata * lata_preco
-apenas_galao = math.ceil(square_meters_qtd/galao_rendimento)    
+apenas_galao = math.ceil(metros_quadrados/galao_rendimento)    
 apenas_galao_preco=apenas_galao * galao_preco  
 
-qtd_latas_both = math.floor(square_meters_qtd/lata_rendimento) 
-qtd_galoes_both = math.ceil( (square_meters_qtd % lata_rendimento) / galao_rendimento) 
+qtd_latas_both = math.floor(metros_quadrados/lata_rendimento) 
+qtd_galoes_both = math.ceil( (metros_quadrados % lata_rendimento) / galao_rendimento) 
 latas_preco_both = qtd_latas_both * lata_preco 
 galao_preco_both = qtd_galoes_both * galao_preco 
 print(f"apenas latas. qtd:{apenas_lata} preço: R$ {apenas_lata_preco}") 
